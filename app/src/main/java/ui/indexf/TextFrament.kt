5feec6449ca4c4300cc9db2635117bb2a7e03416
panshengtao <1274218999@qq.com>
2019-05-30 16:16:49 +0800

    /BaseRecyclerViewAdapterHelper

---
+++ app/src/main/java/ui/indexf/TextFrament.kt
@@ -0,0 +1,39 @@
+package ui.indexf
+
+import android.databinding.ViewDataBinding
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import basebata.base.BaseFragment
+import basebata.base.BaseViewModel
+import ui.BR
+import ui.R
+
+/**
+----------------------------------------------------------------------------------------------
+
+ *项目: mkt      ui.indexf
+ *创建: panshengtao  邮箱：1274218999@lecent.com
+ *时间: 2019年05月14日 5:24 PM
+
+
+ *描述:
+--------------------------------------------------------------------------------------------*/
+
+
+class TextFrament :BaseFragment<ViewDataBinding,BaseViewModel>() {
+    override fun initContentView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): Int {
+
+        return R.layout.test
+    }
+
+    override fun initVariableId(): Int {
+        return BR.viewModel
+    }
+
+    override fun initParam() {
+    }
+
+    override fun initData() {
+    }
+}
